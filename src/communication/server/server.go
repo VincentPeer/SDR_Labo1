14b4f7fc13316c5bf87d3f31883d8de0df68dae1
VincentPeer <vincent.peer@heig-vd.ch>
2022-10-14 16:00:01 +0200

    .

--- src/communication/server/server.go
+++ src/communication/server/server.go
@@ -87,19 +87,4 @@ func handleRequest(conn net.Conn) {
 			return
 		}
 	}
-
-	/*	// Make a buffer to hold incoming data.
-		buf := make([]byte, 1024)
-		// Read the incoming connection into the buffer.
-		reqLen, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Error reading:", err.Error())
-		}
-		if reqLen > 0 {
-			fmt.Println("Received data:", string(buf))
-		}
-		// Send a response back to person contacting us.
-		conn.Write([]byte("Message received."))
-		// Close the connection when you're done with it.
-		conn.Close()*/
 }
--- src/ui/ui.go
+++ src/ui/ui.go
@@ -23,6 +23,8 @@ func UserInterface(reader *bufio.Reader, writer *bufio.Writer) {
 
 }
 
+// Gestion du login client
+// Le client presse enter après chaque entrée, et ne doit pas saisir de ',' dans ses données
 func loginClient(reader *bufio.Reader, writer *bufio.Writer) bool {
 	fmt.Println("Enter your username : ")
 	username, readUsernameError := reader.ReadString('\n') // todo gérer le 2ème attribut de retour (err)
@@ -30,7 +32,8 @@ func loginClient(reader *bufio.Reader, writer *bufio.Writer) bool {
 	fmt.Println("Enter your password : ")
 	password, readPasswordError := reader.ReadString('\n')
 
-	_, writeError := writer.WriteString("LOG," + username + "," + password) // todo check err
+	// Envoi formulaire de login
+	_, writeError := writer.WriteString("LOGIN," + username + "," + password) // todo check err
 	writer.Flush()
 	response, responseError := reader.ReadString('\n')
 	if readUsernameError != nil || readPasswordError != nil ||
@@ -58,7 +61,7 @@ func CreateEvent(reader *bufio.Reader, writer *bufio.Writer) bool {
 		if m == "\n" {
 			break
 		}
-		writer.WriteString("EVENT," + eventName + ",")
+		writer.WriteString("CREATE_EVENT," + eventName + ",")
 
 	}
 	return true

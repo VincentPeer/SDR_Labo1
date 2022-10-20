c3c7dfb8001598879a538c588003db7a2eec6482
VincentPeer <vincent.peer@heig-vd.ch>
2022-10-20 20:27:45 +0200

    client comments

--- src/communication/client/main/main.go
+++ src/communication/client/main/main.go
@@ -2,9 +2,8 @@ package main
 
 import "SDR_Labo1/src/communication/client"
 
+// Main function that start a new client
 func main() {
-	conn := client.CreateConnection()
-	client.UserInterface(conn)
-
-	//conn.Close()
+	conn := client.CreateConnection() // Establish a new connection with the server
+	client.UserInterface(conn)        // Start the user interface
 }
